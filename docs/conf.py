8fcadc0431ee534efee4c560f006710a5df7391a
Emlyn Davies <emlyn.john.davies@sintef.no>
2022-03-08 17:08:41 +0100

    update docs config

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,13 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('./pyopia'))
 
+
+# The master toctree document.
+master_doc = 'index'
 
 # -- Project information -----------------------------------------------------
 
@@ -27,8 +30,7 @@ author = 'Emlyn Davies'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
