d794908163b3be4f8b1990c2fae5957fcca7b7b2
Emlyn Davies <emlyn.john.davies@sintef.no>
2022-04-21 09:43:44 +0200

    add exampledata.py to make notebooks easier

---
+++ notebooks/exampledata.py
@@ -0,0 +1,21 @@
+import urllib.request
+
+
+def get_example_silc_image():
+    url = 'https://pysilcam.blob.core.windows.net/test-data/D20181101T142731.838206.silc'
+    filename = 'D20181101T142731.838206.silc'
+    urllib.request.urlretrieve(url, filename)
+    return filename
+
+
+def get_example_model():
+    # -- Download and unzip the model --#
+    import urllib.request
+    url = 'https://github.com/SINTEF/PySilCam/wiki/ml_models/keras_model.zip'
+    model_filename = 'keras_model.zip'
+    urllib.request.urlretrieve(url, model_filename)
+    import zipfile
+    with zipfile.ZipFile(model_filename, 'r') as zipit:
+        zipit.extractall()
+    model_filename = 'keras_model.h5'
+    return model_filename
