819f68df3819e813fe2dae31736d0a741434313d
Emlyn Davies <emlyn.john.davies@sintef.no>
2022-04-21 15:21:58 +0200

    move badly placed imports

--- notebooks/exampledata.py
+++ notebooks/exampledata.py
@@ -1,4 +1,5 @@
 import urllib.request
+import zipfile
 
 
 def get_example_silc_image():
@@ -10,11 +11,9 @@ def get_example_silc_image():
 
 def get_example_model():
     # -- Download and unzip the model --#
-    import urllib.request
     url = 'https://github.com/SINTEF/PySilCam/wiki/ml_models/keras_model.zip'
     model_filename = 'keras_model.zip'
     urllib.request.urlretrieve(url, model_filename)
-    import zipfile
     with zipfile.ZipFile(model_filename, 'r') as zipit:
         zipit.extractall()
     model_filename = 'keras_model.h5'
