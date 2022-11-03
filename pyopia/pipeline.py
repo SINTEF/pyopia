dbc3057d21cefa6afe384f63ae8be340ef342cc2
Emlyn Davies <emlyn.john.davies@sintef.no>
2022-11-03 22:06:02 +0100

    add better printing of pipeline steps

--- pyopia/pipeline.py
+++ pyopia/pipeline.py
@@ -10,7 +10,6 @@ class Pipeline():
         data = self.steps['load']()
         
         cl = self.steps['classifier']()
-        
         cl.load_model()
         
         stats = self.steps['process'](cl, data)
@@ -18,3 +17,18 @@ class Pipeline():
         return stats
         
         # write metastring to h5 file
+        
+    def print_steps(self):
+        
+        from pyopia import __version__ as pyopia_version
+        
+        # an eventual metadata parser could replace this below loop
+        # and format into an appropriate standard
+        print('\n-- Pipeline configuration --\n')
+        print('PyOpia version: ' + pyopia_version + '\n')
+        for key in self.steps.keys():
+            print('Step name: ' + key + ':\n'
+                  + str(type(self.steps[key]))
+                  + '\n' + str(vars(self.steps[key]))
+                  + '\n')
+        print('---------------------------------\n')
