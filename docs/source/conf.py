1f1a86bcfa208428dd74007c6300054da7b93573
Emlyn Davies <emlyn.john.davies@sintef.no>
2022-11-03 07:24:26 +0100

    try autoapi.extension with sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ sys.path.insert(0, os.path.abspath('./pyopia'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.napoleon', 'autoapi.extension']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
