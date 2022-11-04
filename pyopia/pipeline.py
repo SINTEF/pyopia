573143dc2038bf9e718df83bd75a693935f56a12
Emlyn Davies <emlyn.john.davies@sintef.no>
2022-11-04 08:39:21 +0100

    flake8 fixes in pipeline.py

--- pyopia/pipeline.py
+++ pyopia/pipeline.py
@@ -1,28 +1,28 @@
 
 class Pipeline():
-    
+
     def __init__(self, steps):
         self.steps = steps
         self.cl = self.steps['classifier']()
         self.cl.load_model()
         pass
-    
+
     def run(self):
-        
+
         timestamp, imc = self.steps['load']()
-                
+
         stats, imbw, saturation = self.steps['statextract'](timestamp, imc, self.cl)
-        
+
         #stats = self.steps['process'](cl, data)
-        
+
         return stats
-        
+
         # write metastring to h5 file
-        
+
     def print_steps(self):
-        
+
         from pyopia import __version__ as pyopia_version
-        
+
         # an eventual metadata parser could replace this below loop
         # and format into an appropriate standard
         print('\n-- Pipeline configuration --\n')
