ada1d0a45f2ebfa8488862155fc4977d53f0a441
Emlyn Davies <emlyn.john.davies@sintef.no>
2022-11-07 14:41:56 +0100

    18 metadata documenting processing steps (#20)
    * initial pipeline to get stats for a single image
    
    * update Classify docstring
    
    * add better printing of pipeline steps
    
    * extend silcam pipeline example to include optional inputs
    
    * update classify example in docstring
    
    * start to include h5 output in pipeline
    
    * tidy up example pipeline after chat with william
    
    * old docstrings dont support multiple returns. start fixing in silcam subpackage
    
    * add instrument to sphinx config
    
    * add pmg to gitignore

---
+++ pyopia/instrument/silcam.py
@@ -0,0 +1,85 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+from pyopia.process import statextract
+
+
+def timestamp_from_filename(filename):
+    '''get a pandas timestamp from a silcam filename
+
+    Args:
+        filename (string): silcam filename (.silc)
+
+    Returns:
+        timestamp: timestamp from pandas.to_datetime()
+    '''
+
+    # get the timestamp of the image (in this case from the filename)
+    timestamp = pd.to_datetime(os.path.splitext(os.path.basename(filename))[0][1:])
+    return timestamp
+
+
+class SilCamLoad():
+    '''PyOpia pipline-compatible class for loading a single silcam image
+
+    Parameters
+    ----------
+    filename : string
+        silcam filename (.silc)
+
+    Returns
+    -------
+    timestamp : timestamp
+        timestamp from timestamp_from_filename()
+    img : np.array
+        raw silcam image
+    '''
+
+    def __init__(self, filename):
+        self.filename = filename
+        pass
+
+    def __call__(self):
+        timestamp = timestamp_from_filename(self.filename)
+        img = np.load(self.filename, allow_pickle=False)
+        return timestamp, img
+
+
+class SilCamStatExtract():
+    '''PyOpia pipline-compatible class for calling statextract
+
+    Args:
+        minimum_area (int, optional): minimum number of pixels for particle detection. Defaults to 12.
+        threshold (float, optional): threshold for segmentation. Defaults to 0.98.
+        real_time_stats (bool, optional): changed segmentation method
+          (@todo this option for historical reasons and should be changed). Defaults to False.
+        max_coverage (int, optional): percentage of the image that is allowed to be filled by particles. Defaults to 30.
+        max_particles (int, optional): maximum allowed number of particles in an image.
+          exceeding this will discard the image from analysis. Defaults to 5000.
+    '''
+    def __init__(self,
+                 minimum_area=12,
+                 threshold=0.98,
+                 real_time_stats=False,
+                 max_coverage=30,
+                 max_particles=5000):
+
+        self.minimum_area = minimum_area
+        self.threshold = threshold
+        self.real_time_stats = real_time_stats
+        self.max_coverage = max_coverage
+        self.max_particles = max_particles
+        pass
+
+    def __call__(self, timestamp, imc, Classification):
+        stats, imbw, saturation = statextract(timestamp, imc, Classification,
+                                              minimum_area=self.minimum_area,
+                                              threshold=self.threshold,
+                                              real_time_stats=self.real_time_stats,
+                                              max_coverage=self.max_coverage,
+                                              max_particles=self.max_particles)
+        stats['timestamp'] = timestamp
+        stats['saturation'] = saturation
+        return stats
---
+++ pyopia/pipeline.py
@@ -0,0 +1,55 @@
+class Pipeline():
+    '''Processing pipeline class
+
+    further explanation @todo
+    '''
+
+    def __init__(self, steps):
+        self.steps = steps
+        self.cl = self.steps['classifier']()
+
+    def run(self):
+        '''Method for executing the processing pipeline
+
+        Returns:
+            stats (DataFrame): stats DataFrame of particle statistics
+        '''
+
+        timestamp, imc = self.steps['load']()
+
+        stats = self.steps['statextract'](timestamp, imc, self.cl)
+
+        self.steps['output'](stats, steps_to_string(self.steps))
+
+        return stats
+
+    def print_steps(self):
+        '''Print the steps dictionary
+        '''
+
+        # an eventual metadata parser could replace this below printing
+        # and format into an appropriate standard
+        print('\n-- Pipeline configuration --\n')
+        from pyopia import __version__ as pyopia_version
+        print('PyOpia version: ' + pyopia_version + '\n')
+        print(steps_to_string(self.steps))
+        print('\n---------------------------------\n')
+
+
+def steps_to_string(steps):
+    '''Convert pipeline steps dictionary to a human-readable string
+
+    Args:
+        steps (dict): pipeline steps dictionary
+
+    Returns:
+        str: human-readable string of the types and variables
+    '''
+
+    steps_str = '\n'
+    for i, key in enumerate(steps.keys()):
+        steps_str += (str(i + 1) + ') Step: ' + key
+                      + '\n   Type: ' + str(type(steps[key]))
+                      + '\n   Vars: ' + str(vars(steps[key]))
+                      + '\n')
+    return steps_str
