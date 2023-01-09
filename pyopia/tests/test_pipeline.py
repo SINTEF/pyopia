0ae5a5061036edb5ed3f3da8be55ce281bd1e4bb
Emlyn Davies <emlyn.john.davies@sintef.no>
2023-01-09 15:13:53 +0100

    add test_piepline and setup for tests

---
+++ pyopia/tests/test_pipeline.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from glob import glob
+import tempfile
+import os
+
+from pyopia.classify import Classify
+import pyopia.tests.testdata as testdata
+import pyopia.io
+from pyopia.pipeline import Pipeline
+from pyopia.instrument.silcam import SilCamLoad, ImagePrep
+import pyopia.process
+import pyopia.statistics
+
+
+def test_pipeline():
+    with tempfile.TemporaryDirectory() as tempdir:
+        os.makedirs(tempdir, exist_ok=True)
+        tempdir_proc = os.path.join(tempdir, 'proc')
+        os.makedirs(tempdir_proc, exist_ok=True)
+
+        model_path = testdata.get_example_model(tempdir)
+        print('model_path:', model_path)
+
+        filename = testdata.get_example_silc_image(tempdir)
+        print('filename got:', filename)
+
+        files = glob(os.path.join(tempdir, '*.silc'))
+        print('file list available for test:')
+        print(files)
+
+        datafile_hdf = os.path.join(tempdir_proc, 'test')
+
+        threshold = 0.85
+
+        steps = {'classifier': Classify(model_path=model_path),
+                 'load': SilCamLoad(),
+                 'imageprep': ImagePrep(),
+                 'segmentation': pyopia.process.Segment(threshold=threshold),
+                 'statextract': pyopia.process.CalculateStats(),
+                 'output': pyopia.io.StatsH5(datafile_hdf)}
+
+        processing_pipeline = Pipeline(steps, initial_steps=['classifier'])
+
+        for filename in files[:2]:
+            stats = processing_pipeline.run(filename)
+
+        # display metadata in the h5
+        pyopia.io.show_h5_meta(datafile_hdf + '-STATS.h5')
+
+        # load the stats DataFrame from the h5 file
+        stats = pd.read_hdf(datafile_hdf + '-STATS.h5', 'ParticleStats/stats')
+        print('stats header: ', stats.columns)
+        print('Total number of particles: ', len(stats))
+        print('Number of raw images: ', pyopia.statistics.count_images_in_stats(stats))
+
+
+if __name__ == "__main__":
+    test_pipeline()
