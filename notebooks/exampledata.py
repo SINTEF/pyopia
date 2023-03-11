390715150ee7f14ff0176f1d9ccdfcb730a48c85
Alex Nimmo-Smith <alex.nimmo.smith@plymouth.ac.uk>
2023-03-11 00:12:50 +0000

    adds holo_test_data_02

--- notebooks/exampledata.py
+++ notebooks/exampledata.py
@@ -89,6 +89,9 @@ def get_folder_from_holo_repository(foldername="holo_test_data_01"):
     if foldername == "holo_test_data_01":
         url = 'https://drive.google.com/drive/folders/1yNatOaKdWwYQp-5WVEDItoibr-k0lGsP?usp=share_link'
 
+    elif foldername == "holo_test_data_02":
+        url = "https://drive.google.com/drive/folders/1E5iNSyfeKcVMLVe4PNEwF2Q2mo3WVjF5?usp=share_link"
+
     else:
         foldername == "holo_test_data_01"
         url = 'https://drive.google.com/drive/folders/1yNatOaKdWwYQp-5WVEDItoibr-k0lGsP?usp=share_link'
