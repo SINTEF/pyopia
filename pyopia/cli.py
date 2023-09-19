374a8317ac3d5b0c730ffd88d8886ab23ae3f16f
Emlyn Davies <emlyn.john.davies@sintef.no>
2023-09-19 11:05:28 +0200

    implement pyopia process config.toml command

--- pyopia/cli.py
+++ pyopia/cli.py
@@ -4,6 +4,16 @@ PyOPIA top-level code primarily for managing cmd line entry points
 
 import typer
 import toml
+from glob import glob
+import pyopia.background
+import pyopia.classify
+import pyopia.instrument.silcam
+import pyopia.instrument.holo
+import pyopia.io
+import pyopia.pipeline
+import pyopia.plotting
+import pyopia.process
+import pyopia.statistics
 
 app = typer.Typer()
 
@@ -35,14 +45,30 @@ def generate_config(instrument: str, raw_files: str, model_path: str, outfolder:
     '''
     match instrument:
         case 'silcam':
-            from pyopia.instrument.silcam import generate_config
+            pipeline_config = pyopia.instrument.silcam.generate_config(raw_files, model_path, outfolder, output_prefix)
         case 'holo':
-            from pyopia.instrument.holo import generate_config
+            pipeline_config = pyopia.instrument.holo.generate_config(raw_files, model_path, outfolder, output_prefix)
 
-    pipeline_config = generate_config(raw_files, model_path, outfolder, output_prefix)
     config_filename = instrument + "-config.toml"
     with open(config_filename, "w") as toml_file:
         toml.dump(pipeline_config, toml_file)
+
+
+@app.command()
+def process(config_filename: str, ):
+    '''Run a PyOPIA processing pipeline based on given a config.toml
+    '''
+    from pyopia.io import load_toml
+    from pyopia.pipeline import Pipeline
+
+    pipeline_config = load_toml(config_filename)
+
+    files = sorted(glob(pipeline_config['general']['raw_files']))
+
+    processing_pipeline = Pipeline(pipeline_config)
+
+    for filename in files:
+        processing_pipeline.run(filename)
 
 
 if __name__ == "__main__":
