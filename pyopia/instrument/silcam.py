send imref to classifier so we use colour images. fix image scaling of imref in silcam imageprep

pyopia/instrument/silcam.py
@@ -3,9 +3,10 @@ Module containing SilCam specific tools to enable compatability with the :mod:`p
 '''
 
 import os
-
+import cv2
 import numpy as np
 import pandas as pd
+from skimage.exposure import rescale_intensity
 
 
 def timestamp_from_filename(filename):
@@ -36,7 +37,10 @@ def load_image(filename):
     array
         raw image
     '''
-    img = np.load(filename, allow_pickle=False).astype(np.float64)
+    img = np.load(filename, allow_pickle=False)
+    if img.shape[-1] == 1:
+        # In this case, the image must be converted from Bayer to RGB
+        img = cv2.cvtColor(img, cv2.COLOR_BayerBG2RGB)
     return img
 
 
@@ -97,7 +101,8 @@ class ImagePrep():
 
     def __call__(self, data):
         image = data[self.image_level]
-        data['imref'] = image
+        image = rescale_intensity(image, out_range=(0, 255))
+        data['imref'] = np.uint8(image)
         imc = np.float64(image)
 
         # simplify processing by squeezing the image dimensions into a 2D array

pyopia/process.py
@@ -286,7 +286,7 @@ def extract_particles(imc, timestamp, Classification, region_properties,
 
             if Classification is not None:
                 # run a prediction on what type of particle this might be
-                prediction = Classification.proc_predict(roi.astype(np.uint8))
+                prediction = Classification.proc_predict(roi)
                 predictions[int(i), :] = prediction[0]
 
             # add the roi to the HDF5 file
@@ -428,9 +428,9 @@ def statextract(imbw, timestamp, imc,
         imref = np.copy(imc)
         imc = np.zeros((np.shape(imc)[0], np.shape(imc)[1], 3), dtype=np.uint8)
         # Convert from floats in [0, 1] to ints in [0, 255]
-        imc[:, :, 0] = 255 * imref
-        imc[:, :, 1] = 255 * imref
-        imc[:, :, 2] = 255 * imref
+        imc[:, :, 0] = 255 * imref[:, :, 0]
+        imc[:, :, 1] = 255 * imref[:, :, 0]
+        imc[:, :, 2] = 255 * imref[:, :, 0]
         print('WARNING. exportparticles temporarily modified for 2-d images without color!')
 
     stats = extract_particles(imc, timestamp, Classification, region_properties,
@@ -537,7 +537,7 @@ class CalculateStats():
 
     def __call__(self, data):
         print('statextract')
-        stats, saturation = statextract(data['imbw'], data['timestamp'], data['imc'],
+        stats, saturation = statextract(data['imbw'], data['timestamp'], data['imref'],
                                         Classification=data['cl'],
                                         max_coverage=self.max_coverage,
                                         max_particles=self.max_particles,