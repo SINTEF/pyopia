f097806bc2bbf989617591f82ec7cf7503bde0b5
Emlyn Davies <emlyn.john.davies@sintef.no>
2024-08-09 14:16:27 +0200

    move exampledata.py to pyopia module level (#171)
    * move exampledata.py to pyopia module level
    * remove testdata.py
    
    * remove 'if you dont use git' from README because you should install from pypi if you are not a developer

--- pyopia/exampledata.py
+++ pyopia/exampledata.py
@@ -4,7 +4,7 @@ import os
 import gdown
 
 
-def get_file_from_pysilcam_blob(filename):
+def get_file_from_pysilcam_blob(filename, download_directory='./'):
     '''Downloads a specified filename from the pysilcam.blob into the working dir. if it doesn't already exist
 
     only works for known filenames that are on this blob
@@ -15,13 +15,13 @@ def get_file_from_pysilcam_blob(filename):
         known filename on the blob
 
     '''
-    if os.path.exists(filename):
+    if os.path.exists(os.path.join(download_directory, filename)):
         return filename
     url = 'https://pysilcam.blob.core.windows.net/test-data/' + filename
-    urllib.request.urlretrieve(url, filename)
+    urllib.request.urlretrieve(url, os.path.join(download_directory, filename))
 
 
-def get_example_silc_image():
+def get_example_silc_image(download_directory='./'):
     '''calls `get_file_from_pysilcam_blob` for a silcam iamge
 
     Returns
@@ -33,11 +33,11 @@ def get_example_silc_image():
     if os.path.isfile(filename):
         print('Example image already exists. Skipping download.')
         return filename
-    get_file_from_pysilcam_blob(filename)
+    get_file_from_pysilcam_blob(filename, download_directory)
     return filename
 
 
-def get_example_model():
+def get_example_model(download_directory='./'):
     '''Downloads and unzips an example trained CNN model from the pysilcam.blob
     into the working dir. if it doesn't already exist.
 
@@ -47,20 +47,19 @@ def get_example_model():
         model_filename
     '''
     model_filename = 'keras_model.h5'
-    if os.path.isfile(model_filename):
-        print('keras_model.h5 already exists. Skipping download.')
+    if os.path.exists(model_filename):
         return model_filename
     # -- Download and unzip the model --#
     url = 'https://github.com/SINTEF/PySilCam/wiki/ml_models/keras_model.zip'
     model_filename = 'keras_model.zip'
-    urllib.request.urlretrieve(url, model_filename)
-    with zipfile.ZipFile(model_filename, 'r') as zipit:
-        zipit.extractall()
+    urllib.request.urlretrieve(url, os.path.join(download_directory, model_filename))
+    with zipfile.ZipFile(os.path.join(download_directory, model_filename), 'r') as zipit:
+        zipit.extractall(download_directory)
     model_filename = 'keras_model.h5'
-    return model_filename
+    return os.path.join(download_directory, model_filename)
 
 
-def get_example_hologram_and_background():
+def get_example_hologram_and_background(download_directory='./'):
     '''calls `get_file_from_pysilcam_blob` for a raw hologram, and its associated background image.
 
     Returns
@@ -73,8 +72,12 @@ def get_example_hologram_and_background():
     '''
     holo_filename = '001-2082.pgm'
     holo_background_filename = 'imbg-' + holo_filename
-    get_file_from_pysilcam_blob(holo_filename)
-    get_file_from_pysilcam_blob(holo_background_filename)
+    get_file_from_pysilcam_blob(holo_filename, download_directory)
+    get_file_from_pysilcam_blob(holo_background_filename, download_directory)
+
+    holo_filename = os.path.join(download_directory, holo_filename)
+    holo_background_filename = os.path.join(download_directory, holo_background_filename)
+
     return holo_filename, holo_background_filename
 
 
