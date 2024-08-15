27701f2db2a3d14918ce3cb303b4b3b5d263e539
Raymond Nepstad <raymond.nepstad+github@gmail.com>
2024-08-15 16:43:44 +0200

    183 Add masking step to instruments common toolbox (#184)
    Add optional image border masking step to instruments common toolbox

--- pyopia/pipeline.py
+++ pyopia/pipeline.py
@@ -244,6 +244,9 @@ class Data(TypedDict):
     '''Focussed holographic image'''
     imref: float
     '''Refereence background corrected image passed to silcam classifier'''
+    im_masked: float
+    '''Masked raw image with removed potentially noisy border region before further processsing
+    Obtained from e.g. :class:`pyopia.instrument.common.RectangularImageMask`'''
 
 
 def steps_to_string(steps):
