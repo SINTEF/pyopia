12187e581a68d1d39841dfbba1e2a5d035dc9091
Emlyn Davies <emlyn.john.davies@sintef.no>
2024-08-29 10:46:25 +0200

    remove time limit on classification test

--- pyopia/tests/test_classify.py
+++ pyopia/tests/test_classify.py
@@ -59,8 +59,6 @@ def test_match_to_database():
 
             # start a counter of incorrectly classified images
             failed = 0
-            time_limit = len(files) * 0.01
-            t1 = pd.Timestamp.now()
 
             # loop through the database images
             for file in files:
@@ -76,10 +74,6 @@ def test_match_to_database():
 
             # turn failed count into a success percent
             success = 100 - (failed / len(files)) * 100
-
-            t2 = pd.Timestamp.now()
-            td = t2 - t1
-            assert td < pd.to_timedelta(time_limit, 's'), 'Processing time too long.'
 
             return success
 
