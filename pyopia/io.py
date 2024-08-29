7f5f54acb9f2615fa90c4f4277d425fccf0887ac
Emlyn Davies <emlyn.john.davies@sintef.no>
2024-08-29 21:22:01 +0200

    remove xstats indexing

--- pyopia/io.py
+++ pyopia/io.py
@@ -64,9 +64,7 @@ def write_stats(stats,
         if append and os.path.isfile(datafilename + '-STATS.nc'):
             existing_stats = load_stats(datafilename + '-STATS.nc')
             xstats = xarray.concat([existing_stats, xstats], 'index')
-            xstats.index.values[:] = range(0, xstats.index.size)
         elif not append:
-            xstats = xstats.set_index(index="index")
             datafilename += ('-Image-D' +
                              str(xstats['timestamp'][0].values).replace('-', '').replace(':', '').replace('.', '-'))
         encoding = {k: {'dtype': 'str'} for k in ['export name', 'holo_filename'] if k in xstats.data_vars}
@@ -142,7 +140,6 @@ def combine_stats_netcdf_files(path_to_data):
         STATS xarray dataset
     '''
     xstats = xarray.open_mfdataset(os.path.join(path_to_data, '*Image-D*-STATS.nc'), combine='nested', concat_dim='index')
-    xstats = xstats.set_index(range(0, xstats.index.size))
     return xstats
 
 
