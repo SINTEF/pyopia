fadce13f28f23bde2886b03806c777b3bfd9bf61
Emlyn Davies <emlyn.john.davies@sintef.no>
2024-08-29 10:54:05 +0200

    Update exampledata.py

--- pyopia/exampledata.py
+++ pyopia/exampledata.py
@@ -74,16 +74,16 @@ def get_example_model(download_directory='./'):
     string
         model_filename
     '''
-    model_filename = 'silcam240822.keras'
+    model_filename = 'silcam-classification_database_20240822-200-20240829T091048.keras'
     if os.path.exists(model_filename):
         return model_filename
     # -- Download and unzip the model --#
-    url = 'https://pysilcam.blob.core.windows.net/test-data/silcam240822.zip'
-    model_filename = 'silcam240822.zip'
+    url = 'https://pysilcam.blob.core.windows.net/test-data/silcam-classification_database_20240822-200-20240829T091048.zip'
+    model_filename = 'silcam-classification_database_20240822-200-20240829T091048.zip'
     urllib.request.urlretrieve(url, os.path.join(download_directory, model_filename))
     with zipfile.ZipFile(os.path.join(download_directory, model_filename), 'r') as zipit:
         zipit.extractall(download_directory)
-    model_filename = 'silcam240822.keras'
+    model_filename = 'silcam-classification_database_20240822-200-20240829T091048.keras'
     return os.path.join(download_directory, model_filename)
 
 
