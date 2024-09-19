fe8d76ded354eea4f867593f9d201f71398149a1
Emlyn Davies <emlyn.john.davies@sintef.no>
2024-09-19 09:56:46 +0200

    Better documentation of acceptable SilCam filenames (#295)
    * 294  Better documentation of acceptable SilCam filenames

--- pyopia/instrument/silcam.py
+++ pyopia/instrument/silcam.py
@@ -214,7 +214,8 @@ class SilCamLoad():
     :func:`pyopia.instrument.silcam.timestamp_from_filename`
 
     Required keys in :class:`pyopia.pipeline.Data`:
-        - :attr:`pyopia.pipeline.Data.filename`
+        - :attr:`pyopia.pipeline.Data.filename` conforming to the format 'DYYYYmmddTHHMMSS.ffffff.silc'
+        e.g. 'D20240919T074500.183294.silc'
 
     Parameters
     ----------
