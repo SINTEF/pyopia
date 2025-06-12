bac90c61d39b668e5c06cbb966b73b8db605f5cb
Raymond Nepstad <raymond.nepstad+github@gmail.com>
2025-06-12 15:20:30 +0200

    339 Switch to updated CNN classifier model and newer Keras/TF (#344)
    * 339 Switch to updated CNN classifier model and newer Keras/TF
    
    * 339 Add notebook demoing new CNN classifier model
    
    
    ---------
    
    Co-authored-by: Raymond Nepstad <raymond.nepstad@sintef.no>

--- pyopia/tests/test_classify.py
+++ pyopia/tests/test_classify.py
@@ -1,7 +1,7 @@
-'''
+"""
 A high level test for the basic processing pipeline.
 
-'''
+"""
 
 from glob import glob
 import tempfile
@@ -21,48 +21,48 @@ import numpy as np
 import pyopia.instrument.silcam
 
 
-ACCURACY = 39
+ACCURACY = 60
 
 
 def test_match_to_database():
-    '''
+    """
     Basic check of classification prediction against the training database.
     Therefore, if correct positive matches are not high percentages, then something is wrong with the prediction.
 
     @todo include more advanced testing of the classification feks. assert values in a confusion matrix.
-    '''
+    """
 
     # location of the training data
     with tempfile.TemporaryDirectory() as tempdir:
         # location of the training data
-        database_path = os.path.join(tempdir, 'silcam_classification_database')
+        database_path = os.path.join(tempdir, "silcam_classification_database")
 
         exampledata.get_classifier_database_from_pysilcam_blob(database_path)
-        os.makedirs(os.path.join(tempdir, 'model'), exist_ok=True)
-        model_path = exampledata.get_example_model(os.path.join(tempdir, 'model'))
+        os.makedirs(os.path.join(tempdir, "model"), exist_ok=True)
+        model_path = exampledata.get_example_model(os.path.join(tempdir, "model"))
 
         # Load the trained tensorflow model and class names
         cl = pyopia.classify.Classify(model_path=model_path)
         class_labels = cl.class_labels
 
         # class_labels should match the training data
-        classes = sorted(glob(os.path.join(database_path, '*')))
+        classes = sorted(glob(os.path.join(database_path, "*")))
 
         # @todo write a quick check that classes and class_labels agree before doing the proper test.
 
         def correct_positives(category):
-            '''
+            """
             calculate the percentage positive matches for a given category
-            '''
-            print('Checking', category)
+            """
+            print("Checking", category)
             # list the files in this category of the training data
-            files = glob(os.path.join(database_path, category, '*.tiff'))
+            files = glob(os.path.join(database_path, category, "*.tiff"))
 
-            assert len(files) > 50, 'less then 50 files in test data.'
+            assert len(files) > 50, "less then 50 files in test data."
 
             # start a counter of incorrectly classified images
             failed = 0
-            time_limit = len(files) * 0.01
+            time_limit = len(files) * 0.02
             t1 = pd.Timestamp.now()
 
             # loop through the database images
@@ -83,7 +83,7 @@ def test_match_to_database():
 
             t2 = pd.Timestamp.now()
             td = t2 - t1
-            assert td < pd.to_timedelta(time_limit, 's'), 'Processing time too long.'
+            assert td < pd.to_timedelta(time_limit, "s"), "Processing time too long."
 
             return success
 
@@ -92,35 +92,37 @@ def test_match_to_database():
             name = os.path.split(cat)[-1]
             success = correct_positives(name)
             print(name, success)
-            assert success > ACCURACY, (name + ' was poorly classified at only ' + str(success) + 'percent.')
+            assert success > ACCURACY, (
+                name + " was poorly classified at only " + str(success) + "percent."
+            )
 
 
 def test_pipeline_classification():
-    '''Check that the pipeline doesn't change the outcome of the classification.
+    """Check that the pipeline doesn't change the outcome of the classification.
     Do this by putting rois of know classificion (which we know get correctly classified independintly),
     and then use the same model in a pipeline analysing the synthetic image.
-    '''
+    """
 
     with tempfile.TemporaryDirectory() as tempdir:
         # location of the training data
-        database_path = os.path.join(tempdir, 'silcam_classification_database')
+        database_path = os.path.join(tempdir, "silcam_classification_database")
 
         exampledata.get_classifier_database_from_pysilcam_blob(database_path)
-        os.makedirs('model', exist_ok=True)
-        model_path = exampledata.get_example_model('model')
+        os.makedirs("model", exist_ok=True)
+        model_path = exampledata.get_example_model("model")
 
         # Load the trained tensorflow model and class names
         cl = pyopia.classify.Classify(model_path=model_path)
 
         def get_good_roi(category):
-            '''
+            """
             calculate the percentage positive matches for a given category
-            '''
+            """
 
-            print('category', category)
+            print("category", category)
             # list the files in this category of the training data
-            files = sorted(glob(os.path.join(database_path, category, '*.tiff')))
-            print(len(files), 'files')
+            files = sorted(glob(os.path.join(database_path, category, "*.tiff")))
+            print(len(files), "files")
 
             found_match = 0
             # loop through the database images
@@ -136,16 +138,18 @@ def test_pipeline_classification():
 
                 # check if the highest score matches the correct category
                 if cl.class_labels[ind] == category:
-                    print('roi file', file)
+                    print("roi file", file)
                     return img, category
-            assert found_match == 1, f'classifier not finding matching particle for {category}'
+            assert found_match == 1, (
+                f"classifier not finding matching particle for {category}"
+            )
 
         canvas = np.ones((2048, 2448, 3), np.float64)
 
-        rc_shift = int(2048/len(cl.class_labels)/1.5)
+        rc_shift = int(2048 / len(cl.class_labels) / 1.5)
         rc = rc_shift
 
-        classes = sorted(glob(os.path.join(database_path, '*')))
+        classes = sorted(glob(os.path.join(database_path, "*")))
 
         categories = []
 
@@ -155,75 +159,96 @@ def test_pipeline_classification():
             categories.append(category)
             img_shape = np.shape(img)
             rc += rc_shift
-            canvas[rc:rc + img_shape[0], rc: rc + img_shape[1], :] = np.float64(img)
+            canvas[rc: rc + img_shape[0], rc: rc + img_shape[1], :] = np.float64(img)
 
-        settings = {'general': {'raw_files': None,
-                                'pixel_size': 24},
-                    'steps': {'note': 'non-standard pipeline.'}
-                    }
+        settings = {
+            "general": {"raw_files": None, "pixel_size": 24},
+            "steps": {"note": "non-standard pipeline."},
+        }
 
         # Initialise the pipeline class without running anything
-        MyPipeline = pyopia.pipeline.Pipeline(settings=settings, initial_steps='')
+        MyPipeline = pyopia.pipeline.Pipeline(settings=settings, initial_steps="")
 
         # Get the example trained model
         model_path = pyopia.exampledata.get_example_model(os.getcwd())
 
         # Add the classifier step description to settings (i.e. metadata)
-        MyPipeline.settings['steps'].update({'classifier':
-                                            {'pipeline_class': 'pyopia.classify.Classify',
-                                             'model_path': model_path}})
+        MyPipeline.settings["steps"].update(
+            {
+                "classifier": {
+                    "pipeline_class": "pyopia.classify.Classify",
+                    "model_path": model_path,
+                }
+            }
+        )
 
         # Execute the classifier step we defined above
-        MyPipeline.run_step('classifier')
+        MyPipeline.run_step("classifier")
         # This is the same as running:
         # MyPipeline.data['cl'] = pyopia.classify.Classify(model_path=model_path)
         # Note: the classifier step is special in that it's output is specifically data['cl'], rather than other new keys in data
 
-        MyPipeline.data['imraw'] = canvas
-        MyPipeline.data['timestamp'] = pd.Timestamp.now()
-        MyPipeline.data['filename'] = ''
+        MyPipeline.data["imraw"] = canvas
+        MyPipeline.data["timestamp"] = pd.Timestamp.now()
+        MyPipeline.data["filename"] = ""
 
         # Add the imageprep step description
-        MyPipeline.settings['steps'].update({'imageprep':
-                                            {'pipeline_class': 'pyopia.instrument.silcam.ImagePrep',
-                                             'image_level': 'imraw'}})
+        MyPipeline.settings["steps"].update(
+            {
+                "imageprep": {
+                    "pipeline_class": "pyopia.instrument.silcam.ImagePrep",
+                    "image_level": "imraw",
+                }
+            }
+        )
         # Run the step
-        MyPipeline.run_step('imageprep')
+        MyPipeline.run_step("imageprep")
         # This is the same as running:
         # ImagePrep = pyopia.instrument.silcam.ImagePrep(image_level='imraw')
         # MyPipeline.data = ImagePrep(MyPipeline.data)
 
         # Add the segmentation step description
-        MyPipeline.settings['steps'].update({'segmentation':
-                                            {'pipeline_class': 'pyopia.process.Segment',
-                                             'threshold': 1,
-                                             'segment_source': 'im_minimum'}})
+        MyPipeline.settings["steps"].update(
+            {
+                "segmentation": {
+                    "pipeline_class": "pyopia.process.Segment",
+                    "threshold": 1,
+                    "segment_source": "im_minimum",
+                }
+            }
+        )
         # Run the step
-        MyPipeline.run_step('segmentation')
+        MyPipeline.run_step("segmentation")
         # This is the same as running:
         # Segment = pyopia.process.Segment(threshold=settings['steps']['segmentation']['threshold'])
         # data = Segment(data)
 
         # Add the segmentation step description
-        MyPipeline.settings['steps'].update({'statextract':
-                                            {'pipeline_class': 'pyopia.process.CalculateStats',
-                                             'roi_source': 'imref'}}
-                                            )
+        MyPipeline.settings["steps"].update(
+            {
+                "statextract": {
+                    "pipeline_class": "pyopia.process.CalculateStats",
+                    "roi_source": "imref",
+                }
+            }
+        )
 
         # Run the step
-        MyPipeline.run_step('statextract')
+        MyPipeline.run_step("statextract")
         # This is the same as running:
         # CalculateStats = pyopia.process.CalculateStats()
         # data = CalculateStats(data)
 
-        stats = pyopia.statistics.add_best_guesses_to_stats(MyPipeline.data['stats'])
+        stats = pyopia.statistics.add_best_guesses_to_stats(MyPipeline.data["stats"])
 
-        out = [x[12:] for x in stats['best guess'].values]
+        out = [x[12:] for x in stats["best guess"].values]
 
-        print('classes input', categories)
-        print('classes measured', out)
+        print("classes input", categories)
+        print("classes measured", out)
 
-        assert categories == out, 'Classes returned from classifier do not match what was given to the pipeline'
+        assert categories == out, (
+            "Classes returned from classifier do not match what was given to the pipeline"
+        )
 
 
 if __name__ == "__main__":
