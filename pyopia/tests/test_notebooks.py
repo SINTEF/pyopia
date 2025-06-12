5ce8d12fca01fe4d0e469a237f7e5c8c239150bf
Raymond Nepstad <raymond.nepstad@sintef.no>
2025-06-12 12:23:07 +0200

    339 Link classifier notebook in PyOPIA documentation, include in tests

--- pyopia/tests/test_notebooks.py
+++ pyopia/tests/test_notebooks.py
@@ -1,22 +1,22 @@
-'''
+"""
 A high level test for executing the ipynb notebooks in the notebooks folder.
 
 Can only be run from top-level directory (i.e. with 'poetry run pytest -v')
 
-'''
+"""
 
 from nbconvert.preprocessors import ExecutePreprocessor
 import nbformat
-from glob import glob
+from pathlib import Path
 
 
 def test_notebooks():
-    notebooks = sorted(glob('notebooks/*.ipynb'))
-    notebooks.append('docs/notebooks/background_correction.ipynb')
+    notebooks = sorted(Path("notebooks/").rglob("*.ipynb"))
+    notebooks.append("docs/notebooks/background_correction.ipynb")
     for notebook_filename in notebooks:
         with open(notebook_filename) as f:
             nb = nbformat.read(f, as_version=4)
             ep = ExecutePreprocessor()
-            print('running', notebook_filename)
-            ep.preprocess(nb, {'metadata': {'path': 'notebooks/'}})
-            print(notebook_filename, 'complete')
+            print("running", notebook_filename)
+            ep.preprocess(nb, {"metadata": {"path": "notebooks/"}})
+            print(notebook_filename, "complete")
