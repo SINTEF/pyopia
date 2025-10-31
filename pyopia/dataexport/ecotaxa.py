6849d4897247faa99082c5111288cd22981960ce
DanaMaKing <89377425+DanaMaKing@users.noreply.github.com>
2025-10-31 13:55:15 +0100

    Add dictionary with ecotaxa and statsrow names.Add function to append the information for each row.

--- pyopia/dataexport/ecotaxa.py
+++ pyopia/dataexport/ecotaxa.py
@@ -57,6 +57,59 @@ class EcotaxaExporter:
             xstats.to_pandas()
         )
 
+        # Create ecotaxa dataframe, which specified (mostly) pyopia name and types
+        # define this as a dict
+        ecotaxa_dict = {
+            "img_statsrow_name": ("export_name", "str"),  # source: statsrow
+            "img_rank": ("img_rank", "float"),  # source: we will make it exist
+            "object_id": ("export_name", "str"),  # source: statsrow
+            "object_lat": (
+                "latitude",
+                "str",
+            ),  # needs to be in decimal degrees, source: auxillary data
+            "object_lon": (
+                "longitude",
+                "str",
+            ),  # needs to be in decimal degrees, source: auxillary data
+            "object_date": (
+                "timestamp",
+                "float",
+            ),  # UTC, format: YYYYMMDD, source: statsrow timestamp.dt.strftime(%Y%m%d)
+            "object_time": (
+                "timestamp",
+                "float",
+            ),  # UTC, format: HHMMSS, source: statsrow timestamp.dt.strftime(%H%M%S)
+            "object_depth_min": ("Depth", "float"),  # source: statsrow
+            "object_depth_max": ("Depth", "float"),  # source: statsrow
+            "object_major": ("major_axis_length", "float"),  # source: statsrow
+            "object_minor": ("minor_axis_length", "float"),  # source: statsrow
+            "object_circ.": ("equivalent_diameter", "float"),  # source: statsrow
+            "process_id": (
+                "process_id",
+                "float",
+            ),  # if missing will be added by EcoTaxa
+            "process_img_software_version": ("pyopia.__version__", "str"),
+            "process_img_resolution": ("process_img_resolution", "float"),
+            "particle_pixel_size_μm": (
+                "pixel_size",
+                "float",
+            ),  # needs to be converted from pixels to micrometers
+            "process_date": (
+                "process_date",
+                "float",
+            ),  # we will make it exists, datetime.today().strftime('%Y%m%d'),
+            "process_time": (
+                "process_time",
+                "float",
+            ),  # we will make it exist, datetime.now(timezone.utc).strftime("%H%M%S"),
+            "acq_id": ("acq_id", "float"),  # if missing will be added by EcoTaxa
+            "sample_id": ("sample_id", "float"),  # if missing will be added by EcoTaxa
+            "sample_stationid": (
+                "sample_stationid",
+                "str",
+            ),  #  we will make it exist, sintef specific marker
+        }
+
         buffer = io.BytesIO()
         with zipfile.ZipFile(self.export_filename, "w") as zip:
             # Export particle statistics dataframe to buffer as csv
@@ -64,10 +117,20 @@ class EcotaxaExporter:
             buffer.truncate(0)
             self.df_stats_export.to_csv(buffer)
 
+            # go after the loop, with a ecoataxa df instead of df statsexport
+            def statsrow_to_ecoataxarow(statsrow):
+                ecotaxarow = dict()
+                for k, (statsname, statstype) in ecotaxa_dict.items():
+                    ecotaxarow[k] = statsrow.get(statsname)
+                    # type casting
+                return ecotaxarow
+
+            ecotaxarows = []
+
             # Write particle statistics csv to zip file
             buffer.seek(0)
             zip.writestr("particle_statistics.csv", buffer.read())
-
+            # create it outside here
             for _, row in tqdm(
                 self.df_stats_export.iterrows(), total=self.df_stats_export.shape[0]
             ):
@@ -76,6 +139,9 @@ class EcotaxaExporter:
                 particle_image = pyopia.statistics.roi_from_export_name(
                     export_name, self.roi_dir
                 )
+
+                # call it here
+                ecotaxarows.append(statsrow_to_ecoataxarow(row))
 
                 # Save the particle image to the buffer
                 buffer.seek(0)
